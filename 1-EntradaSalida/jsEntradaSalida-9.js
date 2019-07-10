2661bbe428647e602d48cf74f9a99b6d8ca59870
Nacho Jimenez <52410588+neich12@users.noreply.github.com>
2019-07-09 21:33:12 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -7,18 +7,13 @@ function mostrarAumento()
     var numerosueldo;
     var aumento;
     var numeroresultado;
-    var resto;
+ 
 
     numerosueldo = document.getElementById("sueldo").value;
     numerosueldo = parseInt(numerosueldo);
     var numeroresultado = document.getElementById("resultado").value;
     var numeroresultado = parseInt(numeroresultado);
     //alert("lasumaes: " +numerosueldo) EJEMPLO DE ALERT
-
-    var resto = (aumento = 10%)
-    var resultado = (numerosueldo * aumento);
-  
-
-
+    var aumento = (numerosueldo * 0.10);
 	
 }
