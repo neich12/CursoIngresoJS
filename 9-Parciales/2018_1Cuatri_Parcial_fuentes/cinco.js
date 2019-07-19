a64378d5571b057e5a525e73d47b381cec4f5359
Nacho Jimenez <52410588+neich12@users.noreply.github.com>
2019-07-19 12:10:53 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,38 @@
 function mostrar()
 {
+    
+    var sistemaSolar;
+    var mensaje;
+    
+    
+    sistemaSolar = prompt("Ingrese un planeta");
+
+    switch(sistemaSolar) {
+    
+        case "mercurio":
+        case "venus":
+        mensaje ="Aca hace mas calor";
+        break;
+        
+        
+        case "tierra":
+        mensaje="Aca vivimos";
+        break; 
+        
+        
+        case "marte":
+        case "jupiter":
+        case "saturno":
+        case "urano":
+        case "neptuno":
+        case "pluton":
+        mensaje="Aca hace mas frio";
+          
+        
+        default:
+    
+        
+    }
+        {alert (mensaje ="");}
 
 }
